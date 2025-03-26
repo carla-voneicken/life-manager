b23aac3e3bdbe862b7eddc08ea41189e8f9f9681
Carla von Eicken <carlavoneicken@googlemail.com>
2025-03-26 21:08:08 +0100

    Initial Commit

---
+++ Life Manager/Life_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_ManagerApp.swift
+//  Life Manager
+//
+//  Created by Carla von Eicken on 26.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Life_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
