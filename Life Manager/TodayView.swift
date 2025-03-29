f04f7852ad643626152f9d85a7f115bec2fcb795
Carla von Eicken <carlavoneicken@googlemail.com>
2025-03-29 12:07:30 +0100

    Created TodayView file

---
+++ Life Manager/TodayView.swift
@@ -0,0 +1,18 @@
+//
+//  TodayView.swift
+//  Life Manager
+//
+//  Created by Carla von Eicken on 29.03.25.
+//
+
+import SwiftUI
+
+struct TodayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodayView()
+}
